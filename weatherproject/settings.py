22110a43478b5045b6d616c75c02b156e0a9e30e
adrianla117 <alopeza23@fpcoruna.afundacion.org>
2025-04-23 14:16:48 +0200

    pequeña actualizacion settings

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -119,7 +119,13 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'clima/static')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if not DEBUG:
+    import mimetypes
+    mimetypes.add_type("text/css", ".css", True)
