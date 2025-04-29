b349a24e9c548ec3dc6f33ffe97f1e4040654fab
adrianla117 <alopeza23@fpcoruna.afundacion.org>
2025-04-29 15:01:52 +0200

    Migraciones forzadas

--- clima/urls.py
+++ clima/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('registro/', views.registro, name='registro'),
-    #path('forzar-migraciones/', views.forzar_migraciones, name='forzar_migraciones'),
+    path('forzar-migraciones/', views.forzar_migraciones, name='forzar_migraciones'),
 ]
