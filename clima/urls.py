3d0e86e0458dc18a548e9d153d854282f1bcbb71
adrianla117 <alopeza23@fpcoruna.afundacion.org>
2025-05-03 19:12:49 +0200

    Prueba sin inicio sesion

--- clima/urls.py
+++ clima/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('registro/', views.registro, name='registro'),
+    # path('registro/', views.registro, name='registro'),
     path('forzar-migraciones/', views.forzar_migraciones, name='forzar_migraciones'),
     path('generar-estaticos/', views.generar_estaticos, name='generar_estaticos'),
-    path('guardar-ciudad/', views.guardar_ciudad, name='guardar_ciudad'),
-    path('eliminar-ciudad/', views.eliminar_ciudad, name='eliminar_ciudad_ajax'),
+    # path('guardar-ciudad/', views.guardar_ciudad, name='guardar_ciudad'),
+    # path('eliminar-ciudad/', views.eliminar_ciudad, name='eliminar_ciudad_ajax'),
 ]
--- clima/views.py
+++ clima/views.py
@@ -1,11 +1,11 @@
 import requests
 from django.shortcuts import render
-from django.contrib.auth import login
+#from django.contrib.auth import login
 from django.shortcuts import render, redirect
-from .forms import RegistroForm
-from django.contrib.auth.decorators import login_required
-from .models import CiudadFavorita
-from django.http import JsonResponse
+#from .forms import RegistroForm
+#from django.contrib.auth.decorators import login_required
+#from .models import CiudadFavorita
+#from django.http import JsonResponse
 from django.http import HttpResponse
 from django.core.management import call_command
 
@@ -16,7 +16,7 @@ def forzar_migraciones(request):
 def home(request):
     city = request.GET.get('city', 'Vigo')  #busca el parámetro 'city' que viene desde el formulario con el "GET". Ponemos Vigo como default
     weather_data = None             #aquí vamos a guardar los datos del clima
-    favoritas = []
+    #favoritas = []
 
     if city:
         api_key = 'f1d8b93869fc6876f7753af18dc82ccd'  #aquí ponemos la clave que tenemos de OpenWeatherMap
@@ -29,46 +29,46 @@ def home(request):
             weather_data = response.json() #convierte la respuesta en un diccionario de Python con json
 
     #Si el usuario está autenticado, recuperar su favoritas
-    if request.user.is_authenticated:
-        favoritas = CiudadFavorita.objects.filter(usuario=request.user) #Filtra las ciudades favoritas del usuario autenticado
+    #if request.user.is_authenticated:
+        #favoritas = CiudadFavorita.objects.filter(usuario=request.user) #Filtra las ciudades favoritas del usuario autenticado
 
     return render(request, 'clima/home.html', {
         'weather': weather_data,
-        'favoritas': favoritas, #Pasa las ciudades favoritas al template
+        #'favoritas': favoritas, #Pasa las ciudades favoritas al template
     })
 
 
-def registro(request):
-    if request.method == 'POST':
-        form = RegistroForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)  #Inicia sesión automáticamente después del registro
-            return redirect('home')  #Redirige a la vista de inicio después del registro exitoso
-    else:
-        form = RegistroForm()
-    return render(request, 'registration/registro.html', {'form': form})
+# def registro(request):
+#     if request.method == 'POST':
+#         form = RegistroForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)  #Inicia sesión automáticamente después del registro
+#             return redirect('home')  #Redirige a la vista de inicio después del registro exitoso
+#     else:
+#         form = RegistroForm()
+#     return render(request, 'registration/registro.html', {'form': form})
 
-@login_required
-def guardar_ciudad(request): #Vista para guardar la ciudad favorita del usuario
-    if request.method == 'POST':
-        nombre = request.POST.get('nombre_ciudad') #Obtiene el nombre de la ciudad del formulario
-        #Si el usuario está autenticado, guarda la ciudad favorita
-        if nombre:
-            #Evita duplicados
-            existe = CiudadFavorita.objects.filter(usuario=request.user, nombre__iexact=nombre).exists()
-            if not existe:
-                CiudadFavorita.objects.create(usuario=request.user, nombre=nombre) #Crea la ciudad favorita si no existe
-    return redirect('home') #Redirige a la vista de inicio después de guardar la ciudad favorita
+# @login_required
+# def guardar_ciudad(request): #Vista para guardar la ciudad favorita del usuario
+#     if request.method == 'POST':
+#         nombre = request.POST.get('nombre_ciudad') #Obtiene el nombre de la ciudad del formulario
+#         #Si el usuario está autenticado, guarda la ciudad favorita
+#         if nombre:
+#             #Evita duplicados
+#             existe = CiudadFavorita.objects.filter(usuario=request.user, nombre__iexact=nombre).exists()
+#             if not existe:
+#                 CiudadFavorita.objects.create(usuario=request.user, nombre=nombre) #Crea la ciudad favorita si no existe
+#     return redirect('home') #Redirige a la vista de inicio después de guardar la ciudad favorita
 
-@login_required
-def eliminar_ciudad(request):
-    if request.method == 'POST':
-        ciudad_id = request.POST.get('ciudad_id')
-        ciudad = get_object_or_404(CiudadFavorita, id=ciudad_id, usuario=request.user)
-        ciudad.delete()
-        return JsonResponse({'success': True})
-    return JsonResponse({'success': False}, status=400)
+# @login_required
+# def eliminar_ciudad(request):
+#     if request.method == 'POST':
+#         ciudad_id = request.POST.get('ciudad_id')
+#         ciudad = get_object_or_404(CiudadFavorita, id=ciudad_id, usuario=request.user)
+#         ciudad.delete()
+#         return JsonResponse({'success': True})
+#     return JsonResponse({'success': False}, status=400)
 
 
 def generar_estaticos(request): #Vista para generar archivos estáticos
