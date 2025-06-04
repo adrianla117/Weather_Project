5bff179a0cd719b15e6aa9b90cf699e032089008
adrianla117 <alopeza23@fpcoruna.afundacion.org>
2025-06-04 12:25:28 +0200

    Añadir redirecciones para cuando hacemos Log in y Log out

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -135,3 +135,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'clima/static')]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home' #Para redirigir al usuario a la página de inicio después de iniciar sesión
+LOGOUT_REDIRECT_URL = 'home' #Para redirigir al usuario a la página de inicio después de cerrar sesión
