41ca48bb2f53193b8d9e15433cc9deb41b99df48
adrianla117 <alopeza23@fpcoruna.afundacion.org>
2025-06-04 13:27:43 +0200

    Cambios en vista home y en vista guardar_ciudad para poner un maximo de ciudades guardadas y que enseñen la temperatura y condicion

--- clima/urls.py
+++ clima/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('registro/', views.registro, name='registro'),
-    path('forzar-migraciones/', views.forzar_migraciones, name='forzar_migraciones'),
-    path('generar-estaticos/', views.generar_estaticos, name='generar_estaticos'),
     path('guardar-ciudad/', views.guardar_ciudad, name='guardar_ciudad'),
     path('eliminar-ciudad/', views.eliminar_ciudad_ajax, name='eliminar_ciudad_ajax'),
     path('localizador/', views.localizador_view, name='localizador'),
